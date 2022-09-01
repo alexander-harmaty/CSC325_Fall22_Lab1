662d1ad17bcd05bebca85347347d358ad22c47ef
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-09-01 09:53:54 -0400

    init class

---
+++ CSC325_Fall22_Lab1/src/main/java/edu/farmingdale/csc325_fall22_lab1/CSC325_Fall22_Lab1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package edu.farmingdale.csc325_fall22_lab1;
+
+/**
+ *
+ * @author AlexH
+ */
+public class CSC325_Fall22_Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
